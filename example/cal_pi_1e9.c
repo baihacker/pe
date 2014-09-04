bf1b98db1a8074ad7db62005bdf83ad2327cc694
baihacker <bailiangsky@gmail.com>
2014-09-05 04:04:31 +0800

    Add two examples

---
+++ example/cal_pi_1e9.c
@@ -0,0 +1,41 @@
+const int maxp = 10000000;
+#include <pe_base>
+#include <pe_nt>
+#include <pe_threads>
+
+Threads th;
+
+int ans = 0;
+
+void work(int id)
+{
+	const int start = (id - 1) * 1000000;
+	const int end = id * 1000000;
+	int local_ans = 0;
+	
+	th.lock();
+	cerr << "start:\t" << id << endl;
+	th.unlock();
+	
+	for (int i = start; i <= end; ++i)
+	if (is_prime(i)) ++local_ans;		// current is_prime is slow
+	
+	th.lock();
+	cerr << "finish:\t" << id << endl;
+	ans += local_ans;
+	cerr << "global ans:\t" << ans << endl;
+	th.unlock();
+}
+
+int main()
+{
+	init_primes();
+	th.init(work);
+	for (int id = 1; id <= 1000; ++id)
+	{
+		th.wait_for_queue(3);
+		th.add_request(id);
+	}
+	th.wait_for_end();
+	return 0;
+}
---
+++ example/show_primes.c
@@ -0,0 +1,23 @@
+
+const int maxp = 100000;
+#include <pe_base>
+#include <pe_nt>
+
+int main()
+{
+	init_primes();
+	
+	int total = 0;
+	for (int i = 0; i < pcnt; ++i)
+	{
+		const int v = plist[i];
+		if (v > 10000) break;
+		printf("%d ", v);
+		++total;
+	}
+	puts("");
+	
+	printf("%d %d\n", pmpi[4], total);
+	
+	return 0;
+}

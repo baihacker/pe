9db05d1fbdbe2c7d348d0f0aa8e4798f4bd99575
baihacker <bailiangsky@gmail.com>
2014-12-10 14:13:53 +0800

    add persistance.c

---
+++ example/persistance.c
@@ -0,0 +1,19 @@
+const int maxp = 100000;
+#include <pe>
+KVPersistance kv("fff.txt");
+int main()
+{
+  cerr << kv.size() << endl;
+  cerr << kv.storage()[1] << endl;
+  kv.set(8, 9);
+  kv.set(9, 10);
+  
+  int64 sum = 0;
+  kv.visit([&](int64 k, int64 v){
+    sum += v;
+    cerr << k << " " << v << endl;
+    return true;
+  });
+  dbg(sum);
+  return 0;
+}

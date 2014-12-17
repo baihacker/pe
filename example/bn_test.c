fb2dfb0da7481776e11d8d2546ac1e38892a0ae8
baihacker <bailiangsky@gmail.com>
2014-12-17 10:23:38 +0800

    modify big number: introduce ElementType and MUL_TYPE.

---
+++ example/bn_test.c
@@ -0,0 +1,21 @@
+const int maxp = 100000;
+#include <pe>
+
+void performance_test()
+{
+  TimeRecorder tr;
+  int s = 0;
+  for (int j = 0; j < 5; ++j)
+  {
+    bn x = 1;
+    for (int i = 1; i <= 50000; ++i) x *= i;
+    s += x[0];
+  }
+  cerr << s << " " << tr.elapsed().format() << endl;
+}
+
+int main()
+{
+  performance_test();
+  return 0;
+}

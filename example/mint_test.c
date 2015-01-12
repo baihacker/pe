1130c3b994d31d23782cbe65e539b15b3848610a
baihacker <bailiangsky@gmail.com>
2015-01-12 23:35:24 +0800

    detail modification.

--- example/mint_test.c
+++ example/mint_test.c
@@ -1,5 +1,52 @@
 const int maxp = 100000;
 #include <pe>
+template<typename S>
+struct mtype_policy<S, bn>
+{
+  typedef S   ints;
+  typedef bn   intb;
+  static ints add(ints a, ints b, ints mod)
+  {
+    intb c = (intb)a + b;
+    if (c >= mod) c -= mod;
+    return c.ToInt();
+  }
+  static ints sub(ints a, ints b, ints mod)
+  {
+    intb c = (intb)a - b;
+    if (c < 0) c += mod;
+    return c.ToInt();
+  }
+  static ints mul(ints a, ints b, ints mod)
+  {
+    intb c = (intb)a * b % mod;
+    return c.ToInt();
+  }
+};
+
+template<>
+struct mtype_policy<bn, bn>
+{
+  typedef bn   ints;
+  typedef bn   intb;
+  static ints add(ints a, ints b, ints mod)
+  {
+    intb c = (intb)a + b;
+    if (c >= mod) c -= mod;
+    return c.ToInt();
+  }
+  static ints sub(ints a, ints b, ints mod)
+  {
+    intb c = (intb)a - b;
+    if (c < 0) c += mod;
+    return c.ToInt();
+  }
+  static ints mul(ints a, ints b, ints mod)
+  {
+    intb c = (intb)a * b % mod;
+    return c.ToInt();
+  }
+};
 
 void basic()
 {
@@ -13,7 +60,9 @@ void basic()
 
 const int N = 10000000;
 mint3264<1000000007> u[N+5];
-mintd3264 v[N+5];
+//mintd3264 v[N+5];
+//mint<int32_t, bn, 1000000007> v[N+5];
+mint<int32_t, fake_int128, 1000000007> v[N+5];
 void perf()
 {
   TimeRecorder tr;
@@ -22,7 +71,7 @@ void perf()
   u[i] = u[i-1] * i;
   cerr << tr.elapsed().format() << endl;
   tr.record();
-  v[0].MOD = 1000000007;
+  //v[0].MOD = 1000000007;
   v[0] = 1;
   for (int i = 1; i <= N; ++i)
   v[i] = v[i-1] * i;

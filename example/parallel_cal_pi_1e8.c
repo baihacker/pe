58eb47d1bb01ffcdd55ac8aa6610646137b5a0e4
baihacker <bailiangsky@gmail.com>
2015-06-27 18:34:59 +0800

    modify parallel dsl.

--- example/parallel_cal_pi_1e8.c
+++ example/parallel_cal_pi_1e8.c
@@ -4,9 +4,10 @@ const int maxp = 100000;
 int main()
 {
   init_primes();
-  
+
   BEGIN_PARALLEL
     FROM 1 TO 100000000 EACH_BLOCK_IS 10000000 CACHE ""
+    THREADS 10
     MAP
       {
         return is_prime_ex(key);
@@ -16,8 +17,7 @@ int main()
         result += value;
         return result;
       }
-    GO 10
   END_PARALLEL
-  
+
   return 0;
 }

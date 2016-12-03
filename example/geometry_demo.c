a38e3a9f2d7824f83c51b7f9ccb754e2a104ef24
baihacker <bailiangsky@gmail.com>
2016-12-03 15:50:07 -0800

    Add pe_geometry and the corresponding demo.

---
+++ example/geometry_demo.c
@@ -0,0 +1,31 @@
+const int maxp = 100000;
+#include "pe"
+
+template<typename T>
+void demo(T a, T b)
+{
+  cout << a << endl;
+  cout << b << endl;
+  cout << a + b << endl;
+  cout << a - b << endl;
+  cout << (a < b) << endl;
+  cout << (a <= b) << endl;
+  cout << (a > b) << endl;
+  cout << (a >= b) << endl;
+  cout << (a == b) << endl;
+  cout << (a != b) << endl;
+  a.print();
+  a.println();
+  cout << dot(a, b) << endl;
+  //cout << cross(a, b) << endl;
+  cout << norm(a) << endl;
+  cout << norm2(a) << endl;
+  cout << a.norm2() << endl;
+}
+
+int main()
+{
+  demo(Point2D<int>{1, 2}, Point2D<int>{3, 4});
+  demo(Point3D<int>{1, 2, 3}, Point3D<int>{4, 5, 6});
+  return 0;
+}

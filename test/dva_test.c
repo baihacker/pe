b23a994085d372c544346a0a8bde45a1bdb73dad
baihacker <bailiangsky@gmail.com>
2017-10-08 09:39:16 -0700

    Add dva.

---
+++ test/dva_test.c
@@ -0,0 +1,28 @@
+const int maxp = 1000000;
+#include <pe>
+
+int main()
+{
+  init_primes();
+
+  auto orz = prime_pi(10000);
+  dbg(orz[10000]); // 455052511LL
+  
+  orz = prime_pi(100000000);
+  dbg(orz[100000000]); // 5761455LL
+  
+  orz = prime_s1(10000);
+  dbg(orz[10000]);
+  
+  int64 s = 0;
+  for (int i = 2; i <= 10000; ++i) if (is_prime(i)) s += i;
+  dbg(s);
+  
+  orz = prime_s1(1000000);
+  dbg(orz[1000000]);
+  
+  s = 0;
+  for (int i = 2; i <= 1000000; ++i) if (is_prime(i)) s += i;
+  dbg(s);
+  return 0;
+}

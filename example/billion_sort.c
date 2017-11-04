39ff3c7528b249eb1c5e995cc07d59e52eee20b3
baihacker <bailiangsky@gmail.com>
2017-11-04 09:37:34 -0700

    add billion sort

---
+++ example/billion_sort.c
@@ -0,0 +1,22 @@
+const int maxp = 100000;
+#include <pe>
+
+const int64 N = 1000000000;
+LargeMemory lm;
+
+int main()
+{
+  float* data = (float*) lm.allocate(N * sizeof(float));
+  dbg("memory ready");
+
+  for (int i = 0; i < N; ++i) data[i] = 1. * rand() / RAND_MAX;
+  dbg("data ready");
+  
+  TimeRecorder tr;
+  parallel_sort<30>(data, data+N);
+  // sort(data, data+N);
+  dbg("sorted");
+  
+  cerr << tr.elapsed().format() << endl;
+  return 0;
+}

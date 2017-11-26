9bb3e7b5c3f7cad0aefd7c6350586ee8a6ddf263
baihacker <bailiangsky@gmail.com>
2017-11-25 17:00:16 -0800

    Refine print_int128 and its test.

--- test/print_int128_test.c
+++ test/print_int128_test.c
@@ -3,26 +3,15 @@
 namespace print_int128_test {
 SL void print_int128_test() {
   int128 x = 1;
-  //for (int i = 0; i < 100; ++i) cout << x << endl, x <<= 1;
-  for (int i = 0; i < 130; ++i)
+  for (int i = 0; i < 127; ++i)
   {
-    cout << i << endl;
-    cout << -x << endl;
-    cout << x << endl;
-    print_int128(-x); puts("");
-    print_int128(x); puts("");
-    cout << bn(-x) << endl;
-    cout << bn(x) << endl;
+    assert(to_string(x) == bi(x).toString());
+    assert(to_string(-x) == bi(-x).toString());
     x <<= 1;
   }
   x = 0;
-  cout << "zero" << endl;
-  cout << -x << endl;
-  cout << x << endl;
-  print_int128(-x); puts("");
-  print_int128(x); puts("");
-  cout << bn(-x) << endl;
-  cout << bn(x) << endl;
+  assert(to_string(x) == bi(x).toString());
+  assert(to_string(-x) == bi(-x).toString());
 }
 
 PE_REGISTER_TEST(&print_int128_test, "print_int128_test");

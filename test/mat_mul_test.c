b7ad35e48495f41e5c3b17f87519f2d183edede6
Liang Bai <baihacker@google.com>
2017-11-28 07:59:02 -0800

    Refine mat_mul_test.c.

--- test/mat_mul_test.c
+++ test/mat_mul_test.c
@@ -1,18 +1,24 @@
 #include "pe_test.h"
 
 namespace mat_mul_test {
-#if ENABLE_EIGEN
+#if ENABLE_EIGEN && PE_HAS_INT128
 const int K = 500;
 const int64 mod = 1000000007;
 
 SL void mat_mul_test() {
+  vector<int> data;
+  for (int i = 0; i < K; ++i)
+  for (int j = 0; j < K; ++j)
+  data.push_back(j * K + i);
+  vector<int> V;
+  for (int i = 0; i < K; ++i)
+  V.push_back(i);
   {
-    srand(123);
     std::vector<int64> v(K, 0);
     Mat64 m = Mat64::Zero(K, K);
 
-    for (auto& i: v) i = rand();
-    rep(i, 0, K) rep(j, 0, K) m(i, j) = rand();
+    for (int i = 0; i < K; ++i) v[i] = V[i];
+    rep(i, 0, K) rep(j, 0, K) m(i, j) = data[j*K+i];
 
     TimeRecorder tr;
     v = power_mod(m, 4, v, mod);
@@ -20,16 +26,15 @@ SL void mat_mul_test() {
     for (auto i: v) s += i;
     // cout << s << endl;
     // cout << tr.elapsed().format() << endl;
-    assert(s == 253454003092LL);
+    assert(s == 256670487618LL);
     sort(all(v));
   }
   {
-    srand(123);
     std::vector<int128> v(K, 0);
     Mat128 m = Mat128::Zero(K, K);
 
-    for (auto& i: v) i = rand();
-    rep(i, 0, K) rep(j, 0, K) m(i, j) = rand();
+    for (int i = 0; i < K; ++i) v[i] = V[i];
+    rep(i, 0, K) rep(j, 0, K) m(i, j) = data[j*K+i];
 
     TimeRecorder tr;
     v = power_mod(m, 4, v, mod);
@@ -37,16 +42,15 @@ SL void mat_mul_test() {
     for (auto i: v) s += i;
     // cout << s << endl;
     // cout << tr.elapsed().format() << endl;
-    assert(s == 246217069945LL);
+    assert(s == 247446585411LL);
     sort(all(v));
   }
   {
-    srand(123);
     std::vector<mint6464<mod>> v(K, 0);
     MatM<mod> m = MatM<mod>::Zero(K, K);
 
-    for (auto& i: v) i = rand();
-    rep(i, 0, K) rep(j, 0, K) m(i, j) = rand();
+    for (int i = 0; i < K; ++i) v[i] = V[i];
+    rep(i, 0, K) rep(j, 0, K) m(i, j) = data[j*K+i];
 
     TimeRecorder tr;
     v = power_mod(m, 4, v, mod);
@@ -54,16 +58,15 @@ SL void mat_mul_test() {
     for (auto i: v) s += i.value();
     // cout << s << endl;
     // cout << tr.elapsed().format() << endl;
-    assert(s == 246217069945LL);
+    assert(s == 247446585411LL);
     sort(all(v));
   }
   {
-    srand(123);
     std::vector<mintl<int128, mod>> v(K, 0);
     MatML<mod> m = MatML<mod>::Zero(K, K);
 
-    for (auto& i: v) i = rand();
-    rep(i, 0, K) rep(j, 0, K) m(i, j) = rand();
+    for (int i = 0; i < K; ++i) v[i] = V[i];
+    rep(i, 0, K) rep(j, 0, K) m(i, j) = data[j*K+i];
 
     TimeRecorder tr;
     v = power_mod(m, 4, v, mod);
@@ -71,7 +74,7 @@ SL void mat_mul_test() {
     for (auto i: v) s += i.value();
     // cout << s << endl;
     // cout << tr.elapsed().format() << endl;
-    assert(s == 246217069945LL);
+    assert(s == 247446585411LL);
     sort(all(v));
   }
 }

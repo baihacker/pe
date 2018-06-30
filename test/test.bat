1e5866bcd17e5a67d02ee3899fe9144d34136327
baihacker <bailiangsky@gmail.com>
2018-06-29 17:36:20 -0700

    Allow compiler to specify configurations.

--- test/test.bat
+++ test/test.bat
@@ -1,2 +1,2 @@
-pe++.py pe_test.c -r
+pe++.py pe_test.c -r -DENABLE_ASSERT=1 -DTRY_TO_USE_INT128=1 -DENABLE_OPENMP=1
 pause
---
+++ test/test_all_openmp.bat
@@ -0,0 +1,2 @@
+pe++.py pe_test.c -r -DTEST_ALL -DTRY_TO_USE_INT128=0 -DENABLE_OPENMP=1
+pause

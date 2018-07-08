08838722913bda167bae4fbe8878973e03bb6aa3
baihacker <bailiangsky@gmail.com>
2018-07-08 01:19:17 -0700

    1. Support to count pt in circle. 2. Use <> to include libbf. 3. Add sqrti.

--- test/misc_test.c
+++ test/misc_test.c
@@ -17,4 +17,31 @@ SL void misc_test() {
 }
 
 PE_REGISTER_TEST(&misc_test, "misc_test", SMALL);
+
+SL void count_pt_in_circle_test() {
+  #if 1
+  for (int64 i = 1; i <= 10000; ++i) {
+    int64 u = count_pt_in_circle_q1(i);
+    int64 v = count_pt_in_circle_q1_bf(i);
+    if (u != v) {
+      cerr << i << " " << u << " " << v << endl;
+    }
+    assert(u == v);
+  }
+  #endif
+  // 9999999999999907 7853981733966909 7853981733966913
+  for (int64 i = 10000; i <= 100000000000000000; i = i * 10) {
+    for (int64 j = -3; j <= 3; ++j) {
+      int64 target = i + j;
+      int64 u = count_pt_in_circle_q1(target);
+      int64 v = count_pt_in_circle_q1_bf(target);
+      if (u != v) {
+        cerr << target << " " << u << " " << v << endl;
+      }
+      assert(u == v);
+    }
+  }
+}
+
+PE_REGISTER_TEST(&count_pt_in_circle_test, "count_pt_in_circle_test", MEDIUM);
 }

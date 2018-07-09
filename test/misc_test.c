ee6ca1c08bc48e2478815884a311238eb71870cd
baihacker <bailiangsky@gmail.com>
2018-07-08 21:53:32 -0700

    Add sum_sigma0.

--- test/misc_test.c
+++ test/misc_test.c
@@ -44,4 +44,34 @@ SL void count_pt_in_circle_test() {
 }
 
 PE_REGISTER_TEST(&count_pt_in_circle_test, "count_pt_in_circle_test", MEDIUM);
+
+#if PE_HAS_INT128
+SL void sum_sigma0_test() {
+  #if 1
+  for (int64 i = 1; i <= 10000; ++i) {
+    int64 u = sum_sigma0(i);
+    int64 v = sum_sigma0_bf(i);
+    if (u != v) {
+      cerr << i << " " << u << " " << v << endl;
+    }
+    assert(u == v);
+  }
+  #endif
+
+  // 9999999999999907 7853981733966909 7853981733966913
+  for (int64 i = 10000; i <= 100000000000000000; i = i * 10) {
+    for (int64 j = -3; j <= 3; ++j) {
+      int64 target = i + j;
+      int64 u = sum_sigma0(target);
+      int64 v = sum_sigma0_bf(target);
+      if (u != v) {
+        cerr << target << " " << u << " " << v << endl;
+      }
+      assert(u == v);
+    }
+  }
+}
+
+PE_REGISTER_TEST(&sum_sigma0_test, "sum_sigma0_test", BIG);
+#endif
 }

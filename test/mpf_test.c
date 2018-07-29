f01811f6dd46d8804b8aa00e3cdd6fb18b51e19e
baihacker <bailiangsky@gmail.com>
2018-07-29 09:51:51 -0700

    Refine codes.

--- test/mpf_test.c
+++ test/mpf_test.c
@@ -4,7 +4,7 @@ namespace mpf_test {
 #if HAS_MPF
 SL void mpf_test() {
   //cout << Mpf::getDefaultPrec() << endl;
-  Mpf::setDefaultPrec(100);
+  Mpf::setDefaultPrec(200);
 
   Mpf x(1);
   x /= 10;
@@ -16,9 +16,22 @@ SL void mpf_test() {
   
   cout << Mpf("-.1123456789e31").toString(30) << endl;
   cout << Mpf(".1123456789e31").toString(30) << endl;
-  cout << Mpf(100).toString(30) << endl;*/
+  cout << Mpf(100).toString(30) << endl;
+  
+  mpf_t tester;
+  mpf_init(tester);
+  mpf_set_str(tester, "112345678987654321", 10);
+  cout << Mpf("12345678987654321").toString() << endl;
+  cout << mpf_get_d(tester) << endl;
+  cout << mpf_get_prec(tester) << endl;
+  
+  Mpf y(1);
+  y *= 123456789;
+  y *= 1000000000;
+  y += 123456789;
+  cout << y.toString() << endl;*/
 }
 
-PE_REGISTER_TEST(&mpf_test, "mpf_test", SMALL);
+PE_REGISTER_TEST(&mpf_test, "mpf_test", SPECIFIED);
 #endif
 }

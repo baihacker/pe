2f3357777cd0afef1f1efe9419a80b0642631945
baihacker <bailiangsky@gmail.com>
2018-11-25 16:47:07 -0800

    Refine format script.

--- format.py
+++ format.py
@@ -3,7 +3,8 @@ import sys
 import shutil
 import subprocess
 
-CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+CURRENT_DIRECTORY = os.getcwd();
+# BINARY_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 compile_commands_template='[{ "directory": "$(CURRENT_DIRECTORY)", "file": "pe", "output": "X:\\\\TEMP\\\\pe.o", "arguments": ["clang++.exe", "-xc++", "pe", "--driver-mode=g++", "-c", "--std=c++11", "-O3", "-march=native", "-mtune=native", "--target=x86_64-w64-windows-gnu"]}]'
 
@@ -11,6 +12,8 @@ def tidy():
   with open('compile_commands.json', 'wb') as tempf:
     tempf.write(compile_commands_template.replace('$(CURRENT_DIRECTORY)', CURRENT_DIRECTORY.replace('\\','\\\\')))
   os.system('run-clang-tidy.py -checks=-*,google-readability-casting,google-readability-namespace-comments,modernize-use-emplace,hicpp-use-auto,performance-*,misc-unused-parameters -header-filter=pe.* -export-fixes=format-fixes.yaml -fix pe')
+  os.remove('compile_commands.json')
+  os.remove('format-fixes.yaml')
 
 def format():
   for rt, dirs, files in os.walk(CURRENT_DIRECTORY):

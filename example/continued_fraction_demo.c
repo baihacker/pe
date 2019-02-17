fe9eb22dd687e146813186d56547db0582625633
baihacker <bailiangsky@gmail.com>
2019-02-17 08:46:20 -0800

    Refine continued fraction methods: 1. support to compute the first several fraction. 2. Add macro to shorten method name.

--- example/continued_fraction_demo.c
+++ example/continued_fraction_demo.c
@@ -4,16 +4,16 @@ template <typename GBI>
 void demo() {
   vi data{1, 2, 2, 2, 2, 2, 2, 2, 2, 2};
   for (int i = 0; i < 10; ++i) {
-    cout << from_continued_fraction<GBI>(data, i) << endl;
+    cout << from_cf<GBI>(data, i) << endl;
   }
+  cout << from_cf_n<GBI>(data) << endl;
   for (int i = 50; i <= 50; ++i) {
-    cout << i << " " << to_continued_fraction<GBI>(1, 6, i) << endl;
+    cout << i << " " << to_cf<GBI>(1, 6, i) << endl;
   }
 
-  cout << to_continued_fraction<GBI>(1, 2, 10) << endl;
+  cout << to_cf<GBI>(1, 2, 10) << endl;
 
-  cout << from_continued_fraction<GBI>(to_continued_fraction<GBI>(1, 2, 50))
-       << endl;
+  cout << from_cf<GBI>(to_cf<GBI>(1, 2, 50)) << endl;
 }
 
 int main() {

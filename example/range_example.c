5990b3c2dfa32ec9478e0a5852adcf99bffbc44b
baihacker <bailiangsky@gmail.com>
2019-10-19 23:58:11 -0700

    Refine pe_range.

--- example/range_example.c
+++ example/range_example.c
@@ -104,9 +104,71 @@ void test_range() {
   for (auto i : range(mem)) {
     cout << i.first << " " << i.second << endl;
   }
+
+  // 1*2*3*...*9
+  cout << range(1, 10).reduce(1, [](auto a, auto b) { return a * b; }) << endl;
+  cout << range(1, 10).reduce([](auto a, auto b) { return a * b; }) << endl;
+  cout << range(1, 10).reduce(&ru::mul) << endl;
+  cout << range(1, 10).reduce(1, &ru::mul) << endl;
+
+  cout << range(1, 10).preduce(1, [](auto a, auto b) { return a * b; }) << endl;
+  cout << range(1, 10).preduce([](auto a, auto b) { return a * b; }) << endl;
+  cout << range(1, 10).preduce(&ru::mul) << endl;
+  cout << range(1, 10).preduce(1, &ru::mul) << endl;
+
+  // 1*2*3*...*999999
+  const int64 mod = 1000000007;
+  NMod64<mod> t(1);
+  for (int i = 1; i < 1000000; ++i) t *= i;
+  cout << t << endl;
+
+  cout << range(1, 1000000).reduce(1, [](auto a, auto b) {
+    return a * b % mod;
+  }) << endl;
+  cout << range(1, 1000000).reduce([](auto a, auto b) { return a * b % mod; })
+       << endl;
+  cout << range(1, 1000000).reduce(&ru::mul_mod<mod>) << endl;
+  cout << range(1, 1000000).reduce(ru::mul_mod(mod)) << endl;
+
+  cout << range(1, 1000000).preduce(1, [](auto a, auto b) {
+    return a * b % mod;
+  }) << endl;
+  cout << range(1, 1000000).preduce([](auto a, auto b) { return a * b % mod; })
+       << endl;
+  cout << range(1, 1000000).preduce(&ru::mul_mod<mod>) << endl;
+  cout << range(1, 1000000).preduce(ru::mul_mod(mod)) << endl;
+
+  // Reduce on x
+  cout << range(static_cast<const vector<int>&>(x)).reduce([](auto a, auto b) {
+    return a * b;
+  }) << endl;
+  cout << range(x).reduce(1, [](auto a, auto b) { return a * b; }) << endl;
+  cout << range(x).reduce([](auto a, auto b) { return a * b; }) << endl;
+  cout << range(x).reduce(&ru::mul) << endl;
+
+  cout << range(static_cast<const vector<int>&>(x)).preduce([](auto a, auto b) {
+    return a * b;
+  }) << endl;
+  cout << range(x).preduce(1, [](auto a, auto b) { return a * b; }) << endl;
+  cout << range(x).preduce([](auto a, auto b) { return a * b; }) << endl;
+  cout << range(x).preduce(&ru::mul) << endl;
+
+  // Count prime.
+  cout << range(1, 10000000 + 1)
+              .map([](auto a) { return is_prime(a); })
+              .reduce(&ru::add)
+       << endl;
+  cout << range(1, 10000000 + 1).reduce(0, [](auto a, auto b) {
+    return a + is_prime(b);
+  }) << endl;
+  cout << range(1, 10000000 + 1)
+              .preduce(
+                  0, [](auto a, auto b) { return a + is_prime(b); }, &ru::add)
+       << endl;
 }
 
 int main() {
+  pe().maxPrime(2000000).init();
   test_irange();
   test_range();
   return 0;

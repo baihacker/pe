c979145d8d98e14650a0267d56294f210661c1a4
baihacker <bailiangsky@gmail.com>
2020-05-21 23:41:04 -0700

    Change code style.

--- example/sym_poly_example.c
+++ example/sym_poly_example.c
@@ -16,14 +16,15 @@ SymPoly<int64> target = SymPoly<int64>("a^2-b^2-c^2-b c")
 int his[10];
 void dfs(int now, const SymPoly<int64>& p) {
   if (now == 10) {
-    if (p.terms.empty() && his[7] == 1 && his[8] == 0 && his[9] == -1) {
+    if (p.terms().empty() && his[7] == 1 && his[8] == 0 && his[9] == -1) {
       auto aa =
           t1.replace("c1", his[1]).replace("c2", his[2]).replace("c3", his[3]);
       auto bb =
           t2.replace("c4", his[4]).replace("c5", his[5]).replace("c6", his[6]);
       auto cc =
           t3.replace("c7", his[7]).replace("c8", his[8]).replace("c9", his[9]);
-      if (aa.terms.empty() || bb.terms.empty() || cc.terms.empty()) return;
+      if (aa.terms().empty() || bb.terms().empty() || cc.terms().empty())
+        return;
       cout << "a = " << aa << endl;
       cout << "b = " << bb << endl;
       cout << "c = " << cc << endl;

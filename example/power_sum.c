ce03d5559b54ab42c12e071e191681b6336d4248
baihacker <bailiangsky@gmail.com>
2020-11-19 17:47:54 -0800

    Add more power sum mod implementation.

--- example/power_sum.c
+++ example/power_sum.c
@@ -3,6 +3,10 @@
 const int64 mod = 1000000007;
 using MT = NMod64<mod>;
 
+using SumfunctionType = int64 (*)(int64 n, int64 mod);
+SumfunctionType sum_function[8] = {nullptr,   &P1SumMod, &P2SumMod, &P3SumMod,
+                                   &P4SumMod, &P5SumMod, &P6SumMod, &P7SumMod};
+
 int main() {
   PE_INIT(maxp = 2000000);
   PowerSumModer moder0(mod);    // maxk = 128
@@ -20,6 +24,10 @@ int main() {
       cout << "PowerSumModer     " << moder0.Cal(n, k) << endl;
       cout << "PowerSumModerB    " << moder1.Cal(n, k) << endl;
       cout << "PowerSumModerB1   " << moder2.Cal(n, k) << endl;
+      if (k >= 1 && k <= 7) {
+        cout << "P" << k << "SumMod          " << sum_function[k](n, mod)
+             << endl;
+      }
       cout << endl;
     }
     cout << endl;

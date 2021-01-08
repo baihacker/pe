6e2164b9cb2db838e9de611d60a881112d06b8c6
baihacker <bailiangsky@gmail.com>
2021-01-07 20:32:53 -0800

    1. Refine comments. 2. Add one more DfaCounter example in which set_count_each_len(1) is required.

--- example/dfa_counter.c
+++ example/dfa_counter.c
@@ -11,27 +11,50 @@ int64 check(int64 n) {
 int64 bf(int64 n) { return XRange(1LL, n).Map<int64>(check).Sum(); }
 
 int main() {
-  PE_INIT(maxp=1000000);
+  PE_INIT(maxp = 1000000);
 
   // state 0: initial state
   // state 1: 3 is not seen
   // state 2: 3 is seen
-  DfaCounter<int64> counter;
-  counter.Init(3, 10, 16);
+  DfaCounter<int64> counter1;
+  counter1.Init(3, 10, 16);
   for (int i = 1; i <= 9; ++i)
-    if (i != 3) counter.AddTrans(0, i, 1);
-  counter.AddTrans(0, 3, 2);
-  counter.AddTrans(0, 0, 0);
+    if (i != 3) counter1.AddTrans(0, i, 1);
+  counter1.AddTrans(0, 3, 2);
+  counter1.AddTrans(0, 0, 0);
   for (int i = 0; i <= 9; ++i)
-    if (i != 3) counter.AddTrans(1, i, 1);
-  counter.AddTrans(1, 3, 2);
-  for (int i = 0; i <= 9; ++i) counter.AddTrans(2, i, 2);
-  counter.MarkAsCount(2);
+    if (i != 3) counter1.AddTrans(1, i, 1);
+  counter1.AddTrans(1, 3, 2);
+  for (int i = 0; i <= 9; ++i) counter1.AddTrans(2, i, 2);
+  counter1.MarkAsCount(2);
+
+  // In counter2, the dfa doesn't accept leading zeros, in other words, if it's
+  // initial state and 0 comes, the targe state is invalid. So we need to call
+  // set_count_each_len(1).
+  //
+  // state 0: initial state
+  // state 1: 3 is not seen
+  // state 2: 3 is seen
+  // state 3: invalid
+  DfaCounter<int64> counter2;
+  counter2.Init(4, 10, 16);
+  for (int i = 1; i <= 9; ++i)
+    if (i != 3) counter2.AddTrans(0, i, 1);
+  counter2.AddTrans(0, 3, 2);
+  for (int i = 0; i <= 9; ++i)
+    if (i != 3) counter2.AddTrans(1, i, 1);
+  counter2.AddTrans(1, 3, 2);
+  for (int i = 0; i <= 9; ++i) counter2.AddTrans(2, i, 2);
+  counter2.MarkAsCount(2);
+  counter2.AddTrans(0, 0, 3);
+  for (int i = 0; i <= 9; ++i) counter2.AddTrans(3, i, 3);
+  counter2.set_count_each_len(1);
 
   for (int64 n = 10; n <= 1000000; n *= 10) {
     int64 a = bf(n);
-    int64 b = counter.Cal(n);
-    cout << n << "\t" << a << "\t" << b << endl;
+    int64 b = counter1.Cal(n);
+    int64 c = counter2.Cal(n);
+    cout << n << "\t" << a << "\t" << b << "\t" << c << endl;
   }
   return 0;
 }

1b30e13bfa94e884e9d2a2cb1c8d60b7f4ff23d4
baihacker <bailiangsky@gmail.com>
2021-06-12 21:09:30 -0700

    1. Add DfaSummer and its example. 2. Refine DfaCounter and its example.

--- example/dfa_counter.c
+++ example/dfa_counter.c
@@ -2,13 +2,13 @@
 
 // Count the number which contains 3.
 
-int64 check(int64 n) {
+int64 cal(int64 n) {
   for (; n; n /= 10)
     if (n % 10 == 3) return 1;
   return 0;
 }
 
-int64 bf(int64 n) { return XRange(1LL, n).Map<int64>(check).Sum(); }
+int64 bf(int64 n) { return XRange(1LL, n).Map<int64>(cal).Sum(); }
 
 int main() {
   PE_INIT(maxp = 1000000);
@@ -26,7 +26,7 @@ int main() {
     if (i != 3) counter1.AddTrans(1, i, 1);
   counter1.AddTrans(1, 3, 2);
   for (int i = 0; i <= 9; ++i) counter1.AddTrans(2, i, 2);
-  counter1.MarkAsCount(2);
+  counter1.MarkTargetState(2);
 
   // In counter2, the dfa doesn't accept leading zeros, in other words, if it's
   // initial state and 0 comes, the targe state is invalid. So we need to call
@@ -45,15 +45,15 @@ int main() {
     if (i != 3) counter2.AddTrans(1, i, 1);
   counter2.AddTrans(1, 3, 2);
   for (int i = 0; i <= 9; ++i) counter2.AddTrans(2, i, 2);
-  counter2.MarkAsCount(2);
+  counter2.MarkTargetState(2);
   counter2.AddTrans(0, 0, 3);
   for (int i = 0; i <= 9; ++i) counter2.AddTrans(3, i, 3);
   counter2.set_count_each_len(1);
 
   for (int64 n = 10; n <= 1000000; n *= 10) {
-    int64 a = bf(n);
-    int64 b = counter1.Cal(n);
-    int64 c = counter2.Cal(n);
+    auto a = bf(n);
+    auto b = counter1.Cal(n);
+    auto c = counter2.Cal(n);
     cout << n << "\t" << a << "\t" << b << "\t" << c << endl;
   }
   return 0;
---
+++ example/dfa_summer.c
@@ -0,0 +1,63 @@
+#include <pe.hpp>
+
+// Compute the 3rd power of the number which contains 3.
+const int64 mod = 1000000007;
+using MT = NMod64<mod>;
+
+int64 cal(int64 n) {
+  const int64 m = n;
+  for (; n; n /= 10)
+    if (n % 10 == 3) return m * m % mod * m % mod;
+  return 0;
+}
+
+int64 bf(int64 n) { return XRange(1LL, n).Map<int64>(cal).SumMod(mod); }
+
+int main() {
+  PE_INIT(maxp = 1000000);
+
+  // state 0: initial state
+  // state 1: 3 is not seen
+  // state 2: 3 is seen
+  DfaSummer<MT> summer1;
+  summer1.Init(3, 3, 10, 16);
+  for (int i = 1; i <= 9; ++i)
+    if (i != 3) summer1.AddTrans(0, i, 1);
+  summer1.AddTrans(0, 3, 2);
+  summer1.AddTrans(0, 0, 0);
+  for (int i = 0; i <= 9; ++i)
+    if (i != 3) summer1.AddTrans(1, i, 1);
+  summer1.AddTrans(1, 3, 2);
+  for (int i = 0; i <= 9; ++i) summer1.AddTrans(2, i, 2);
+  summer1.MarkTargetState(2);
+
+  // In summer2, the dfa doesn't accept leading zeros, in other words, if it's
+  // initial state and 0 comes, the targe state is invalid. So we need to call
+  // set_count_each_len(1).
+  //
+  // state 0: initial state
+  // state 1: 3 is not seen
+  // state 2: 3 is seen
+  // state 3: invalid
+  DfaSummer<MT> summer2;
+  summer2.Init(4, 3, 10, 16);
+  for (int i = 1; i <= 9; ++i)
+    if (i != 3) summer2.AddTrans(0, i, 1);
+  summer2.AddTrans(0, 3, 2);
+  for (int i = 0; i <= 9; ++i)
+    if (i != 3) summer2.AddTrans(1, i, 1);
+  summer2.AddTrans(1, 3, 2);
+  for (int i = 0; i <= 9; ++i) summer2.AddTrans(2, i, 2);
+  summer2.MarkTargetState(2);
+  summer2.AddTrans(0, 0, 3);
+  for (int i = 0; i <= 9; ++i) summer2.AddTrans(3, i, 3);
+  summer2.set_count_each_len(1);
+
+  for (int64 n = 10; n <= 1000000; n *= 10) {
+    auto a = bf(n);
+    auto b = summer1.Cal(n);
+    auto c = summer2.Cal(n);
+    cout << n << "\t" << a << "\t" << b << "\t" << c << endl;
+  }
+  return 0;
+}

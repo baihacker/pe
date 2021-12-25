14f3ba4e6f948fab1e8f1f23cc32657119552fff
baihacker <bailiangsky@gmail.com>
2021-12-25 08:42:20 -0800

    1. Refactor extended int. 2. Move extended_int_test.c to extended_unsigned_int_test.c

--- test/pe_test.c
+++ test/pe_test.c
@@ -1,7 +1,7 @@
 #include "pe_test.h"
 
-#if 0
-#include "extended_int_test.c"
+#if 1
+#include "extended_unsigned_int_test.c"
 #else
 #include "algo_test.c"
 #include "array_test.c"
@@ -10,7 +10,7 @@
 #include "bit_test.c"
 #include "comb_moder_test.c"
 #include "dva_test.c"
-#include "extended_int_test.c"
+#include "extended_unsigned_int_test.c"
 #include "fact_ppower_mod_test.c"
 #include "init_inv_test.c"
 #include "int128_test.c"

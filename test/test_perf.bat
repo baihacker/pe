8d224316d3592ea4abc4011e0a8450f0e940c6a3
baihacker <bailiangsky@gmail.com>
2023-01-01 22:09:59 -0800

    Update benchmark result.

--- test/test_perf.bat
+++ test/test_perf.bat
@@ -1,2 +1,2 @@
-pe++.py test_perf.c -DTEST_ALL -DENABLE_ASSERT=0 -DTRY_TO_USE_INT128=1 -DENABLE_OPENMP=1 && a.exe -r 7
+pe++.py test_perf.c -DTEST_ALL -DENABLE_ASSERT=0 -DTRY_TO_USE_INT128=1 -DENABLE_OPENMP=1 && a.exe -r 10
 pause

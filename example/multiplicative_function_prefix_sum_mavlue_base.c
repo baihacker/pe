ff4ea8df431d1488669cbde8d65d089cebd09b8f
baihacker <bailiangsky@gmail.com>
2025-02-09 00:45:03 -0800

    Merge multiplicative_function_prefix_sum_mavlue_base_perf.c to multiplicative_function_prefix_sum_mavlue_base.c and refine codes.

--- example/multiplicative_function_prefix_sum_mavlue_base.c
+++ example/multiplicative_function_prefix_sum_mavlue_base.c
@@ -14,8 +14,14 @@ struct Solver1 : public MValueBase<Solver1, RT, 8> {
   RT BatchF(int imp, int64 vmp, int64 remain) {
     return F(2, 1) * RT(dva[remain] - (imp + 1));
   }
-  void Init(int64 n) { dva = PrimeS0Ex<int64>(n); }
+  void Init(int64 n) {
+    dva = PrimeS0Ex<int64>(n);
+    tr.Record();
+  }
+  void Done() { usage = tr.Elapsed(); }
   DVA<int64> dva;
+  TimeRecorder tr;
+  TimeDelta usage;
 };
 
 struct Solver2 : public MValueBase<Solver2, RT, 8> {
@@ -40,8 +46,14 @@ struct Solver2 : public MValueBase<Solver2, RT, 8> {
   RT BatchF(int imp, int64 vmp, int64 remain) {
     return F(2, 1) * RT(dva[remain] - (imp + 1));
   }
-  void Init(int64 n) { dva = PrimeS0Ex<int64>(n); }
+  void Init(int64 n) {
+    dva = PrimeS0Ex<int64>(n);
+    tr.Record();
+  }
+  void Done() { usage = tr.Elapsed(); }
   DVA<int64> dva;
+  TimeRecorder tr;
+  TimeDelta usage;
 };
 
 struct Solver3 : public MValueBase<Solver3, RT, 8, true> {
@@ -66,13 +78,56 @@ struct Solver3 : public MValueBase<Solver3, RT, 8, true> {
   RT BatchF(int imp, int64 vmp, int64 remain) {
     return F(2, 1) * RT(dva[remain] - (imp + 1));
   }
-  void Init(int64 n) { dva = PrimeS0Ex<int64>(n); }
+  void Init(int64 n) {
+    dva = PrimeS0Ex<int64>(n);
+    tr.Record();
+  }
+  void Done() { usage = tr.Elapsed(); }
   DVA<int64> dva;
+  TimeRecorder tr;
+  TimeDelta usage;
+};
+
+struct Solver4 : public MValueVisitor<Solver4, RT, 8> {
+  RT Visit(int64 n, int64 val, int imp, int64 vmp, int emp, MVVHistory* his,
+           int top) {
+    RT ret = 0;
+    int64 remain = n / val;
+    RT now = 1;
+    RT now1 = 1;
+    for (int i = 0; i < top; ++i) {
+      now *= his[i].e + 1;
+      if (his[i].p != vmp) {
+        now1 *= his[i].e + 1;
+      }
+    }
+    // we have remain >= vmp
+    // handle val * q where q > vmp
+    if (remain > vmp) {
+      ret += now * 2 * RT(dva[remain] - (imp + 1));
+    }
+    if (val > 1) {
+      // handle val * vmp
+      ret += now1 * (emp + 2);
+    } else {
+      // handle f(1)
+      ret += 1;
+    }
+    return ret;
+  }
+  void Init(int64 n) {
+    dva = PrimeS0Ex<int64>(n);
+    tr.Record();
+  }
+  void Done() { usage = tr.Elapsed(); }
+  DVA<int64> dva;
+  TimeRecorder tr;
+  TimeDelta usage;
 };
 
 DVA<int64> dva;
-MVVHistory history_[128];
-int top_;
+MVVHistory history[128];
+int top;
 RT Dfs(int limit, int64 n, int64 val, int imp, int64 vmp, int emp, RT now,
        RT now1) {
   RT ret = 0;
@@ -97,41 +152,162 @@ RT Dfs(int limit, int64 n, int64 val, int imp, int64 vmp, int emp, RT now,
     const int64 next_vmp = imp == -1 ? p : vmp;
     const int64 val_limit = n / p / next_vmp;
     if (val > val_limit) break;
-    history_[top_].ip = i;
-    history_[top_].p = p;
-    history_[top_].e = 1;
-    int& e = history_[top_++].e;
+    history[top].ip = i;
+    history[top].p = p;
+    history[top].e = 1;
+    history[top].pd = p;
+    int& e = history[top].e;
+    int64& pd = history[top++].pd;
     for (int64 nextval = val * p;; ++e) {
       RT tmp = now * (e + 1);
       ret += Dfs(i, n, nextval, next_imp, next_vmp, imp == -1 ? e : emp, tmp,
                  imp == -1 ? 1 : now1 * (e + 1));
       if (nextval > val_limit) break;
+      pd *= p;
       nextval *= p;
     }
-    --top_;
+    --top;
   }
   return ret;
 }
 
-int main() {
-  PE_INIT(maxp = 2000000);
+RT Dfs1(int start, int limit, int64 n, int64 val, int imp, int64 vmp, int emp,
+        RT now, RT now1) {
+  RT ret = 0;
+  {
+    int64 remain = n / val;
+    // we have remain >= vmp
+    // handle val * q where q > vmp
+    if (remain > vmp) {
+      ret += now * (dva[remain] - (imp + 1)) * 2;
+    }
+    if (val > 1) {
+      // handle val * vmp
+      ret += now1 * (emp + 2);
+    } else {
+      // handle f(1)
+      ret += 1;
+    }
+  }
+  for (int i = start; i < limit; ++i) {
+    const int64 p = plist[i];
+    const int64 val_limit = n / p / p;
+    if (val > val_limit) break;
+    history[top].ip = i;
+    history[top].p = p;
+    history[top].e = 1;
+    history[top].pd = p;
+    int& e = history[top].e;
+    int64& pd = history[top++].pd;
+    for (int64 nextval = val * p;; ++e) {
+      RT tmp = now * (e + 1);
+      ret += Dfs1(i + 1, limit, n, nextval, i, p, e, tmp, now);
+      if (nextval > val_limit) break;
+      pd *= p;
+      nextval *= p;
+    }
+    --top;
+  }
+  return ret;
+}
 
+void RunCorrectnessTest() {
   TableFormatter tf;
-  for (int64 n = 1; n <= 100000000; n *= 10) {
-    int64 a = Solver0().Cal(n);
-    int64 b = Solver1().Cal(n);
-    int64 c = Solver2().Cal(n);
-    int64 d = Solver3().Cal(n);
+  auto& line = tf.AppendLine();
+  line.push_back("n");
+  line.push_back("Solver0");
+  line.push_back("Solver1");
+  line.push_back("Solver2");
+  line.push_back("Solver3");
+  line.push_back("Solver4");
+  line.push_back("Dfs");
+  line.push_back("Dfs1");
+  for (int64 n = 1, e = 0; n <= 100000000; n *= 10, ++e) {
+    int64 a0 = Solver0().Cal(n);
+    int64 a1 = Solver1().Cal(n);
+    int64 a2 = Solver2().Cal(n);
+    int64 a3 = Solver3().Cal(n);
+    int64 a4 = Solver4().Cal(n);
     ::dva = PrimeS0Ex<int64>(n);
-    int64 e = Dfs(pcnt, n, 1, -1, 1, 0, 1, 1);
+    int64 a5 = Dfs(pcnt, n, 1, -1, 1, 0, 1, 1);
+    int64 a6 = Dfs1(0, pcnt, n, 1, -1, 1, 0, 1, 1);
     auto& line = tf.AppendLine();
-    line.push_back(pe::ToString(n));
-    line.push_back(pe::ToString(a));
-    line.push_back(pe::ToString(b));
-    line.push_back(pe::ToString(c));
-    line.push_back(pe::ToString(d));
-    line.push_back(pe::ToString(e));
+    line.push_back("1e" + pe::ToString(e));
+    line.push_back(pe::ToString(a0));
+    line.push_back(pe::ToString(a1));
+    line.push_back(pe::ToString(a2));
+    line.push_back(pe::ToString(a3));
+    line.push_back(pe::ToString(a4));
+    line.push_back(pe::ToString(a5));
+    line.push_back(pe::ToString(a6));
   }
   tf.Render(std::cout);
+}
+
+void RunPerfTest() {
+  TableFormatter tf;
+  auto& line = tf.AppendLine();
+  line.push_back("n");
+  line.push_back("Solver2");
+  line.push_back("Solver3");
+  line.push_back("Solver4");
+  line.push_back("Dfs");
+  line.push_back("Dfs1");
+  for (int64 n = 1000000, e = 6; n <= 1000000000000; n *= 10, ++e) {
+    // std::cout << "n = " << n << std::endl;
+    auto& line = tf.AppendLine();
+    line.push_back("1e" + pe::ToString(e));
+    {
+      Solver2 solve;
+      auto ans = solve.Cal(n);
+      // std::cout << ans << std::endl;
+      // std::cout << solve.usage.Format() << std::endl;
+      line.push_back(solve.usage.Format());
+    }
+
+    {
+      Solver3 solve;
+      auto ans = solve.Cal(n);
+      // std::cout << ans << std::endl;
+      // std::cout << solve.usage.Format() << std::endl;
+      line.push_back(solve.usage.Format());
+    }
+
+    {
+      Solver4 solve;
+      auto ans = solve.Cal(n);
+      // std::cout << ans << std::endl;
+      // std::cout << solve.usage.Format() << std::endl;
+      line.push_back(solve.usage.Format());
+    }
+
+    {
+      ::dva = PrimeS0Ex<int64>(n);
+      TimeRecorder tr;
+      auto ans = Dfs(pcnt, n, 1, -1, 1, 0, 1, 1);
+      // std::cout << ans << std::endl;
+      // std::cout << tr.Elapsed().Format() << std::endl;
+      line.push_back(tr.Elapsed().Format());
+    }
+
+    {
+      ::dva = PrimeS0Ex<int64>(n);
+      TimeRecorder tr;
+      auto ans = Dfs1(0, pcnt, n, 1, -1, 1, 0, 1, 1);
+      // std::cout << ans << std::endl;
+      // std::cout << tr.Elapsed().Format() << std::endl;
+      line.push_back(tr.Elapsed().Format());
+    }
+
+    // std::cout << std::endl;
+  }
+
+  tf.Render(std::cout);
+}
+int main() {
+  PE_INIT(maxp = 2000000);
+  RunCorrectnessTest();
+  std::cout << std::endl;
+  RunPerfTest();
   return 0;
 }

017deb6aca22d606fdd26a9597d0714bbb1301ab
baihacker <bailiangsky@gmail.com>
2025-03-29 19:24:14 -0700

    Refine codes.

--- pe.hpp
+++ pe.hpp
@@ -2,7 +2,8 @@
 #define PE_HPP_
 
 // Use
-// g++ -xc++-header "pe.hpp" --std=c++17 -O3 -march=native -fopenmp
+// g++ -xc++-header pe.hpp --std=c++20 -fno-diagnostics-color -O3 -march=native
+// -mtune=native -fopenmp -pthread -static
 // to generate pe.hpp.gch
 #include <pe>
 

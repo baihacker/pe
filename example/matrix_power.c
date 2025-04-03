b7d891346d91c55c828789792549ae85f8dfe29d
baihacker <bailiangsky@gmail.com>
2025-04-02 23:02:16 -0700

    Matrix power example should respect ENABLE_EIGEN.

--- example/matrix_power.c
+++ example/matrix_power.c
@@ -37,6 +37,7 @@ int64 solve1(int64 n) {
   return ans[0].value();
 }
 
+#if ENABLE_EIGEN
 // Mod is associated with T at runtime.
 // Different threads use different mod.
 int64 solve2(int64 n, int64 rmod) {
@@ -54,6 +55,7 @@ int64 solve2(int64 n, int64 rmod) {
   Eigen::setNbThreads(0);
   return ans[0].value();
 }
+#endif
 
 // Mod is associated with T at runtime.
 // All the threads use the same mod.
@@ -98,12 +100,16 @@ int main() {
     std::cout << "n = " << n << std::endl;
     int64 ans0 = solve0(n);
     int64 ans1 = solve1(n);
+#if ENABLE_EIGEN
     int64 ans2 = solve2(n, mod);
+#endif
     int64 ans3 = solve3(n, mod);
     int64 ans4 = solve4(n, mod);
     std::cout << "Ans0 = " << ans0 << std::endl;
     std::cout << "Ans1 = " << ans1 << std::endl;
+#if ENABLE_EIGEN
     std::cout << "Ans2 = " << ans2 << std::endl;
+#endif
     std::cout << "Ans3 = " << ans3 << std::endl;
     std::cout << "Ans4 = " << ans4 << std::endl;
     std::cout << std::endl;
@@ -114,10 +120,14 @@ int main() {
       const int64 n = 1000000000;
       std::cout << "n = " << n << std::endl;
       std::cout << "rmod = " << rmod << std::endl;
+#if ENABLE_EIGEN
       int64 ans2 = solve2(n, rmod);
+#endif
       int64 ans3 = solve3(n, rmod);
       int64 ans4 = solve4(n, rmod);
+#if ENABLE_EIGEN
       std::cout << "Ans2 = " << ans2 << std::endl;
+#endif
       std::cout << "Ans3 = " << ans3 << std::endl;
       std::cout << "Ans4 = " << ans4 << std::endl;
       std::cout << std::endl;

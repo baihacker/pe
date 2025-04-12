ba16915bb963bd4a199a460af1817e53e66bbadb
baihacker <bailiangsky@gmail.com>
2025-04-12 14:10:43 -0700

    Refine precompile.bat

--- precompile.bat
+++ precompile.bat
@@ -1,2 +1,2 @@
-g++ -xc++-header pe.hpp --std=c++20 -fno-diagnostics-color -O3 -march=native -mtune=native -fopenmp -pthread -static
+g++ -xc++-header pe.hpp --std=c++20 -fno-diagnostics-color -O2 -march=native -mtune=native -fopenmp -pthread -static
 pause

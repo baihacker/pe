21d80879ff530a37c6bfbe99eae426baa54efc0e
baihacker <bailiangsky@gmail.com>
2025-04-13 12:02:36 -0700

    Move CombModerTest to algo_test.c

--- test/algo_test.c
+++ test/algo_test.c
@@ -104,6 +104,29 @@ SL void FactSumModTest() {
 }
 PE_REGISTER_TEST(&FactSumModTest, "FactSumModTest", BIG);
 
+SL void CombModerTest() {
+  const int mod = 10007;
+  CombModer m1(mod);
+  CombModerEx m2(mod);
+  for (int i = 0; i < 10000; ++i) {
+    const int n = rand() % mod;
+    const int m = rand() % mod;
+    assert(m1.Comb(n, m) == m2.Comb(n, m));
+  }
+#if PE_HAS_INT128
+  {
+    const int64 mod = 99999999907;
+    CombModerEx moder(mod);
+    const int64 n = 66666666604;
+    const int64 m = 33333333302;
+    auto result = moder.Comb(n, m);
+
+    assert(result == 99999410307);
+  }
+#endif
+}
+PE_REGISTER_TEST(&CombModerTest, "CombModerTest", MEDIUM);
+
 SL void PowerSumTest() {
   assert(PowerSumMod(10, 2, 1000000007) == 385);
   assert(PowerSumMod(100, 100, 1000000007) == 568830579);

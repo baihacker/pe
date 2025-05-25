13f6e34de9c5cde33d4db885fc4577269ad19f97
baihacker <bailiangsky@gmail.com>
2025-05-24 18:34:20 -0700

    Refine codes.

--- test/gbi_test.c
+++ test/gbi_test.c
@@ -361,14 +361,17 @@ class GbiTests {
       assert(GetBit(a, 120) == 1);
       RevBit(a, 120);
       assert(IsZero(a));
-      RevBit(a, 240);
-      assert(BitWidth(a) == 241);
+    }
+    {
+      TestT a;
+      RevBit(a, 120);
+      assert(BitWidth(a) == 121);
       assert(Popcount(a) == 1);
-      for (int i = 0; i < 240; ++i) {
+      for (int i = 0; i < 120; ++i) {
         assert(GetBit(a, i) == 0);
       }
-      assert(GetBit(a, 240) == 1);
-      ResetBit(a, 240);
+      assert(GetBit(a, 120) == 1);
+      ResetBit(a, 120);
       assert(IsZero(a));
     }
   }

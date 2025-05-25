81e44ea7777f13f16b29c24a3a9fbeb4b5d4bd07
baihacker <bailiangsky@gmail.com>
2025-05-25 10:30:51 -0700

    Refine continued fraction.

--- example/continued_fraction.c
+++ example/continued_fraction.c
@@ -1,20 +1,22 @@
 #include "pe.hpp"
 using namespace pe;
 
-template <typename GBI>
+template <typename T>
 void demo() {
   std::vector<int> data{1, 2, 2, 2, 2, 2, 2, 2, 2, 2};
   for (int i = 0; i < 10; ++i) {
-    std::cout << FromCf<GBI>(data, i) << std::endl;
+    std::cout << FromCf<T>(data, i) << std::endl;
   }
-  std::cout << FromCfN<GBI>(data) << std::endl;
+  std::cout << FromCfN<T>(data) << std::endl;
   for (int i = 50; i <= 50; ++i) {
-    std::cout << i << " " << ToCf<GBI>(1, 6, i) << std::endl;
+    std::cout << i << " " << ToCf<T>(0, 1, 6, 1, i) << std::endl;
   }
 
-  std::cout << ToCf<GBI>(1, 2, 10) << std::endl;
+  std::cout << ToCf<T>(0, 1, 2, 1, 10) << std::endl;
+  std::cout << FromCf<T>(ToCf<T>(0, 1, 2, 1, 50)) << std::endl;
 
-  std::cout << FromCf<GBI>(ToCf<GBI>(1, 2, 50)) << std::endl;
+  std::cout << ToCf<T>(123456, 654321) << std::endl;
+  std::cout << FromCf<T>(ToCf<T>(123456, 654321)) << std::endl;
 }
 
 int main() {

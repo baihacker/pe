a515f972a5276e81a34a244b2eff774d80eba98a
baihacker <bailiangsky@gmail.com>
2025-07-03 21:59:39 -0700

    Update multiplicative_function_prefix_sum_common_function by adding ntf implementation.

--- example/multiplicative_function_prefix_sum_common_function.c
+++ example/multiplicative_function_prefix_sum_common_function.c
@@ -12,20 +12,39 @@ int main() {
   Sigma0Summer<int128> sigma0_summer;
   MuPhiSumModer mu_phi_sum_moder(mod);
   Sigma0SumModer sigma0_sum_moder(mod);
-
+  std::string suffx = std::to_string(mod);
   for (int64 n = 1; n <= 10000000000; n *= 10) {
-    std::cout << n << " = " << n << std::endl;
-    std::cout << "# square free number " << sf_counter.Cal(n) << std::endl;
-    std::cout << "sum mu " << mu_summer.Cal(n) << std::endl;
-    std::cout << "sum mu " << mu_phi_summer.CalSumMu(n) << std::endl;
-    std::cout << "sum phi " << mu_phi_summer.CalSumPhi(n) << std::endl;
-    std::cout << "sum sigma0 " << sigma0_summer.Cal(n) << std::endl;
+    std::cout << "n = " << n << std::endl;
 
-    std::cout << "sum mu mod " << mod << " " << mu_phi_sum_moder.CalSumMu(n)
+    DVAPrefixSumMaker<int128> maker(n);
+    std::cout << std::left;
+    std::cout << std::setw(32) << "# square free number " << sf_counter.Cal(n)
               << std::endl;
-    std::cout << "sum phi mod " << mod << " " << mu_phi_sum_moder.CalSumPhi(n)
+    std::cout << std::setw(32) << "# square free number " << maker.MuSquare()[n]
               << std::endl;
-    std::cout << "sum sigma0 mod " << mod << " " << sigma0_sum_moder.Cal(n)
+    std::cout << std::setw(32) << "sum mu " << mu_summer.Cal(n) << std::endl;
+    std::cout << std::setw(32) << "sum mu " << mu_phi_summer.CalSumMu(n)
+              << std::endl;
+    std::cout << std::setw(32) << "sum mu " << maker.Mu()[n] << std::endl;
+    std::cout << std::setw(32) << "sum phi " << mu_phi_summer.CalSumPhi(n)
+              << std::endl;
+    std::cout << std::setw(32) << "sum phi " << maker.Phi()[n] << std::endl;
+    std::cout << std::setw(32) << "sum sigma0 " << sigma0_summer.Cal(n)
+              << std::endl;
+    std::cout << std::setw(32) << "sum sigma0 " << maker.D0()[n] << std::endl;
+
+    DVAPrefixSumMaker<NModCC64<mod>> maker_mod(n);
+    std::cout << std::setw(32) << "sum mu mod " + suffx
+              << mu_phi_sum_moder.CalSumMu(n) << std::endl;
+    std::cout << std::setw(32) << "sum mu mod " + suffx << maker_mod.Mu()[n]
+              << std::endl;
+    std::cout << std::setw(32) << "sum phi mod " + suffx
+              << mu_phi_sum_moder.CalSumPhi(n) << std::endl;
+    std::cout << std::setw(32) << "sum mu mod " + suffx << maker_mod.Phi()[n]
+              << std::endl;
+    std::cout << std::setw(32) << "sum sigma0 mod " + suffx
+              << sigma0_sum_moder.Cal(n) << std::endl;
+    std::cout << std::setw(32) << "sum mu mod " + suffx << maker_mod.D0()[n]
               << std::endl;
     std::cout << std::endl;
   }
